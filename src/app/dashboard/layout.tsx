a6a9ad5817d882cc3ab78d5cbe9d28309ad5ce97
Soymaferlopezp <0xmaferlopez@gmail.com>
2025-09-18 15:32:42 -0400

    update

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -2,6 +2,8 @@
 
 import Sidebar from "@/components/Sidebar";
 
+export const dynamic = 'force-dynamic';
+
 export default function DashboardLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="min-h-screen bg-space text-pure flex">
